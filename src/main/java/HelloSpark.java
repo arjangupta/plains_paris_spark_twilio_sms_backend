caef6ca88a3cfb656adafa16233aff573e77c5ba
Arjan Gupta <arjangupta95@gmail.com>
2020-08-16 15:36:46 -0500

    Instantiate TwilioRestClient

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -2,10 +2,15 @@ import com.twilio.http.TwilioRestClient;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
-import static spark.Spark.*;
+import spark.Spark;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class HelloSpark {
     public static void main(String[] args) {
         get("/", (req, res) -> "Test: This is the SMS backend of Plains Paris Referral App");
     }
+
+    TwilioRestClient client = new TwilioRestClient.Builder(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN")).build();
 }
