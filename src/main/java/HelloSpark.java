e79cc45a98c494cdb0c046cf88ea4364c45a700d
Arjan Gupta <arjangupta95@gmail.com>
2020-08-16 15:52:33 -0500

    Add the POST request to send the texts

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -2,15 +2,29 @@ import com.twilio.http.TwilioRestClient;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
-import spark.Spark;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class HelloSpark {
     public static void main(String[] args) {
-        get("/", (req, res) -> "Test: This is the SMS backend of Plains Paris Referral App");
-    }
+        get("/", (req, res) -> "This is the SMS backend of Plains Paris Referral App - READY TO SEND TEXTS");
 
-    TwilioRestClient client = new TwilioRestClient.Builder(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN")).build();
+        // Instantiate REST client
+        TwilioRestClient client = new TwilioRestClient.Builder(System.getenv("AC6d2d5e845b62dbaa240a0d85c65a1726"), System.getenv("e22d6bef569003091aa4673d234e6fd2")).build();
+
+        // Add a POST request endpoint
+        post("/sms", (req, res) -> {
+            String body = req.queryParams("Body");
+            String to = req.queryParams("To");
+            String from = "+12057935447";
+
+            Message message = new MessageCreator(
+                    new PhoneNumber(to),
+                    new PhoneNumber(from),
+                    body).create(client);
+
+            return message.getSid();
+        });
+    }
 }
