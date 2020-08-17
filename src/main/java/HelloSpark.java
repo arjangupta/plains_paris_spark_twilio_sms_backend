b68f10b271bc4535d2e655aa10ab4a7e4fde3998
Arjan Gupta <arjangupta95@gmail.com>
2020-08-16 22:16:04 -0500

    Try out Twilio's dashboard Java example for sending an SMS

--- src/main/java/HelloSpark.java
+++ src/main/java/HelloSpark.java
@@ -1,6 +1,5 @@
-import com.twilio.http.TwilioRestClient;
+import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
 
 import static spark.Spark.get;
@@ -8,10 +7,10 @@ import static spark.Spark.post;
 
 public class HelloSpark {
     public static void main(String[] args) {
-        get("/", (req, res) -> "This is the SMS backend of Plains Paris Referral App - READY TO SEND TEXTS");
+        get("/", (req, res) -> "SMS Backend v0.1 of Plains Paris Referral App");
 
         // Instantiate REST client
-        TwilioRestClient client = new TwilioRestClient.Builder(System.getenv("AC6d2d5e845b62dbaa240a0d85c65a1726"), System.getenv("e22d6bef569003091aa4673d234e6fd2")).build();
+        Twilio.init("AC6d2d5e845b62dbaa240a0d85c65a1726", "e22d6bef569003091aa4673d234e6fd2");
 
         // Add a POST request endpoint
         post("/sms", (req, res) -> {
@@ -19,10 +18,10 @@ public class HelloSpark {
             String to = req.queryParams("To");
             String from = "+12057935447";
 
-            Message message = new MessageCreator(
+            Message message = Message.creator(
                     new PhoneNumber(to),
                     new PhoneNumber(from),
-                    body).create(client);
+                    body).create();
 
             return message.getSid();
         });
